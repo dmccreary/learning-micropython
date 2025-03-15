c3872d7d4be5903e450d044cb74c496f12cc973d
Dan Mccreary <Dan.McCreary@gmail.com>
2025-03-15 15:31:23 -0500

    updating content

--- src/kits/wide-fft/04-gray-scale-test.py
+++ src/kits/wide-fft/04-gray-scale-test.py
@@ -1,38 +1,25 @@
-import time
+# display grascale test
+
 from machine import Pin, SPI
-from utime import sleep
-import ssd1322v2
+import ssd1322
+import config
 
-DISPLAY_WIDTH = 256
-DISPLAY_HEIGHT = 64
+spi = SPI(config.SPI_BUS, baudrate=10_000_000, sck=Pin(config.SPI_SCK_PIN), mosi=Pin(config.SPI_SDA_PIN))
+disp = ssd1322.SSD1322_SPI(256, 64, spi, dc=Pin(config.SPI_DC_PIN), cs=Pin(config.SPI_CS_PIN), res=Pin(config.SPI_RES_PIN))
+disp.set_orientation(h_flip=False, v_flip=True, nibble_remap=False)
 
-# Create Pin objects
-scl = Pin(2)
-sda = Pin(3)
-res = Pin(4, Pin.OUT)
-dc = Pin(5, Pin.OUT)
-cs = Pin(6, Pin.OUT)
-
-# Initialize with lower baud rate
-spi = SPI(0, baudrate=1_000_000, sck=scl, mosi=sda)
-
-# Initialize display
-disp = ssd1322v2.SSD1322_SPI(256, 64, spi, dc, cs, res)
+DISPLAY_WIDTH = config.DISPLAY_WIDTH
+DISPLAY_HEIGHT = config.DISPLAY_HEIGHT
 
 # Basic test sequence
 print("Starting display grayscale test...")
 
-# disp.rotate(0x06)
-
-# the left side is off by 15
-x_left = 15
-
-for i in range(0,16):
-    # x1, y1, x2, y2, col
-    # draw 4 vertical lines of the same shade
-    disp.line(x_left, i*4,   DISPLAY_WIDTH-1, i*4,   i)
-    disp.line(x_left, i*4+1, DISPLAY_WIDTH-1, i*4+1, i)
-    disp.line(x_left, i*4+2, DISPLAY_WIDTH-1, i*4+2, i)
-    disp.line(x_left, i*4+3, DISPLAY_WIDTH-1, i*4+3, i)
+for i in range(0,15):
+    # line(x1, y1, x2, y2, color)
+    # draw 4 horizontal lines of the same shade
+    disp.line(0, i*4,   DISPLAY_WIDTH-1, i*4,   i)
+    disp.line(0, i*4+1, DISPLAY_WIDTH-1, i*4+1, i)
+    disp.line(0, i*4+2, DISPLAY_WIDTH-1, i*4+2, i)
+    disp.line(0, i*4+3, DISPLAY_WIDTH-1, i*4+3, i)
 disp.show()
 
